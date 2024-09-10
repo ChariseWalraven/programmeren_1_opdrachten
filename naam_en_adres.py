67324e85f24cfa45af357513f327091c9c7aad2c
Charise Walraven <31189070+ChariseWalraven@users.noreply.github.com>
2024-09-10 15:09:20 +0200

    wk1 2e opdracht afmaken

---
+++ naam_en_adres.py
@@ -0,0 +1,13 @@
+# Write your solution here
+voornaam = input('wat is je voornaam?')
+achternaam = input('wat is je achternaam?')
+straatdres = input('wat is je straatadres (inclusief huisnr)?')
+stad_postcode = input('wat is je stad en postcode?')
+
+
+res = f"""Voornaam: {voornaam}
+Achternaam: {achternaam}
+Straatadres: {straatdres}
+Stad en postcode: {stad_postcode}"""
+
+print(res)
